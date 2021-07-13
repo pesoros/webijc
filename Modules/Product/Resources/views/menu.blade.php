07e97bddaa9abd5902149de7441c0a43b1e5bf92
bayuyuhartono-katadata <bayu.yuhartono@katadata.co.id>
2021-07-13 14:13:15 +0700

    sidebarprd

--- Modules/Product/Resources/views/menu.blade.php
+++ Modules/Product/Resources/views/menu.blade.php
@@ -20,7 +20,7 @@
             </a>
             <ul>
 
-                @if(permissionCheck('add_product.create'))
+                {{-- @if(permissionCheck('add_product.create')) --}}
                 <li>
                     <a href="{{route('add_product.create')}}" class="{{request()->is('product/add_product/create') ? 'active' : ''}}"> {{__('common.Product List')}} </a>
                 </li>
@@ -28,41 +28,41 @@
                 <li>
                     <a href="{{route('add_product.service')}}" class="{{request()->is('product/add_product/service') ? 'active' : ''}}"> {{__('product.Service')}} </a>
                 </li>
-                @endif
-                @if(permissionCheck('add_product.index'))
+                {{-- @endif --}}
+                {{-- @if(permissionCheck('add_product.index')) --}}
                 <li>
                     <a href="{{route('add_product.index')}}" class="{{request()->is('product/add_product') ? 'active' : ''}}"> {{__('common.Add Product')}} </a>
                 </li>
-                @endif
+                {{-- @endif --}}
                
-                @if(permissionCheck('category.index'))
+                {{-- @if(permissionCheck('category.index')) --}}
                 <li>
                     <a href="{{route('category.index')}}" class="{{request()->is('product/category') ? 'active' : ''}}"> {{__('common.Category')}} </a>
                 </li>
-                @endif
-                @if(permissionCheck('brand.index'))
+                {{-- @endif --}}
+                {{-- @if(permissionCheck('brand.index')) --}}
                 <li>
                     <a href="{{route('brand.index')}}" class="{{request()->is('product/brand') ? 'active' : ''}}"> {{__('common.Brand')}} </a>
                 </li>
-                @endif
+                {{-- @endif --}}
 
-                @if(permissionCheck('model.index'))
+                {{-- @if(permissionCheck('model.index')) --}}
                 <li>
                     <a href="{{route('model.index')}}" class="{{request()->is('product/model') ? 'active' : ''}}"> {{__('common.Model')}} </a>
                 </li>
-                @endif
+                {{-- @endif --}}
 
-                @if(permissionCheck('unit_type.index'))
+                {{-- @if(permissionCheck('unit_type.index')) --}}
                 <li>
                     <a href="{{route('unit_type.index')}}" class="{{request()->is('product/unit_type') ? 'active' : ''}}"> {{__('common.Unit Type')}}</a>
                 </li>
-                @endif
+                {{-- @endif --}}
 
-                @if(permissionCheck('variant.index'))
+                {{-- @if(permissionCheck('variant.index')) --}}
                 <li>
                     <a href="{{route('variant.index')}}" class="{{request()->is('product/variant') ? 'active' : ''}}"> {{__('common.Variant')}}</a>
                 </li>
-                @endif
+                {{-- @endif --}}
             </ul>
         </li>
 
